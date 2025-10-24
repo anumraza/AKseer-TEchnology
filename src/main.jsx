d3212c9caaac1efb17781befb44b9db9391597a8
Anum Fatima <anumraza8194@gmail.com>
2025-10-24 23:08:27 +0500

    update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode >
+  <React.StrictMode basename="/AKseer-TEchnology/">
     < HelmetProvider >
       <App />
     </HelmetProvider >
